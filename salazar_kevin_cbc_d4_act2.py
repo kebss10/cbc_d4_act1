60b8e9ec6f03e7893a5ccd327eaf350a8c4302c2
kebss10 <kevin.salazar@aclcbutuan.edu.ph>
2025-02-22 11:09:52 +0800

    first commit

--- salazar_kevin_cbc_d4_act2.py
+++
@@ -1,9 +0,0 @@
-words = str(input("Input a word: "))
-
-reverse = words.replace(" ", "")
-palindrome = reverse[::-1]
-
-if palindrome == reverse:
-    print("This is a palindrome:", words)
-else:
-    print("This is not a palindrome:", words)
