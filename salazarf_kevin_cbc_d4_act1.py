047d01044dd7625c34e07b43ef77e98c4fd4b45e
kebss10 <kevin.salazar@aclcbutuan.edu.ph>
2025-02-22 10:59:44 +0800

    first commit

---
+++ salazarf_kevin_cbc_d4_act1.py
@@ -0,0 +1,21 @@
+num1 = 50
+num2 = 25
+
+print("Sddition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:",num1 * num2)
+print("Division:", num1 / num2)
+print("Modulus:", num1 % num2)
+print("Exponentiation:",num1 ** num2)
+print("Floor division:",num1 // num2)
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 < num2)
+print(num1 > num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+print(num1 and num2)
+print(num1 or num2)
+print(not num1)
